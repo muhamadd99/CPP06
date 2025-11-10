4984e1e8e098d63962a042affd28868c85f191d4
muha.madd99@yahoo.com <muha.madd99@yahoo.com>
2025-11-10 22:47:18 +0800

    understanding why Scalar converter

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,9 +6,12 @@
 /*   By: mbani-ya <mbani-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 20:04:12 by mbani-ya          #+#    #+#             */
-/*   Updated: 2025/11/10 20:06:46 by mbani-ya         ###   ########.fr       */
+/*   Updated: 2025/11/10 22:42:47 by mbani-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+#include <string>
 
 class ScalarConverter {
 private:
@@ -17,5 +20,6 @@ private:
 	ScalarConverter& operator=(const ScalarConverter& other);
 	~ScalarConverter();
 public:
-
-}
+	static void convert(const std::string& Param);
+	
+};
