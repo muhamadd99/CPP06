a4bdc4b51fcfdf92a8b8c6ba9d76dcbe786d15e2
Muhammad Bani Yamin <mbani-ya@u90z05s01.42kl.edu.my>
2025-11-10 20:16:42 +0800

    added folders

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbani-ya <mbani-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 20:04:12 by mbani-ya          #+#    #+#             */
+/*   Updated: 2025/11/10 20:06:46 by mbani-ya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class ScalarConverter {
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter& operator=(const ScalarConverter& other);
+	~ScalarConverter();
+public:
+
+}
